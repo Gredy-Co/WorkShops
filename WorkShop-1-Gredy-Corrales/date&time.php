96bede4f89f0bd65ead79076c4983b6c54410cf1
Gredy-Co <greddycorrales.m@gmail.com>
2024-09-16 23:48:23 -0600

    Se agrega el proyecto

---
+++ WorkShop-1-Gredy-Corrales/date&time.php
@@ -0,0 +1,7 @@
+<?php
+// Establece la zona horaria a Costa Rica
+date_default_timezone_set('America/Costa_Rica');
+
+// Muestra la fecha y hora actual
+echo "Current date and time in Costa Rica: " . date('Y-m-d H:i:s');
+?>
